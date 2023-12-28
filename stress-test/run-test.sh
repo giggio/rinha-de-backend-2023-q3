ab222c6c418b589deee912e2910fbfc1a9fbf9df
Giovanni Bassi <giggio@giggio.net>
2023-12-28 13:17:25 -0300

    temp

--- stress-test/run-test.sh
+++ stress-test/run-test.sh
@@ -1,12 +1,14 @@
+#!/usr/bin/env sh
+
 # Exemplos de requests
 # curl -v -XPOST -H "content-type: application/json" -d '{"apelido" : "xpto", "nome" : "xpto xpto", "nascimento" : "2000-01-01", "stack": null}' "http://localhost:9999/pessoas"
 # curl -v -XGET "http://localhost:9999/pessoas/1"
 # curl -v -XGET "http://localhost:9999/pessoas?t=xpto"
 # curl -v "http://localhost:9999/contagem-pessoas"
 
-GATLING_BIN_DIR=$HOME/gatling/3.9.5/bin
+a="/$0"; a="${a%/*}"; a="${a:-.}"; a="${a##/}/"; WORKSPACE=$(cd "$a"; pwd)
 
-WORKSPACE=$HOME/projects/rinha-de-backend-2023-q3/stress-test
+GATLING_BIN_DIR=$WORKSPACE/.gatling/bin
 
 sh $GATLING_BIN_DIR/gatling.sh -rm local -s RinhaBackendSimulation \
     -rd "DESCRICAO" \
